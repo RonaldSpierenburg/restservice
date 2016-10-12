bba97c91f8fa4124e9f45373bc7fd009c7988e11
Ronald Spierenburg <ronald.spierenburg@luminis.eu>
2016-10-12 15:48:07 +0200

    first commit

---
+++ src/nl/luminis/javatraining/ExampleApplication.java
@@ -0,0 +1,9 @@
+package nl.luminis.javatraining;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class ExampleApplication extends Application {
+ 
+}
---
+++ src/nl/luminis/javatraining/pojo/Person.java
@@ -0,0 +1,30 @@
+package nl.luminis.javatraining.pojo;
+
+import java.io.Serializable;
+
+public class Person implements Serializable {
+
+	private String firstName;
+	private String lastName;
+	
+	public Person(String aFirstName, String aLastName) {
+		setFirstName(aFirstName);
+		setLastName(aLastName);
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+}
